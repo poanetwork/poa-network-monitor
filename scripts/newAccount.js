f7102bacff89f5a6fd7eb6da0f91bf2436c6c936
Natalia Gulko <natalia.gulko@yahoo.com>
2018-06-18 18:38:11 -0700

    Added password as cmd parameter in the newAccount script

--- scripts/newAccount.js
+++ scripts/newAccount.js
@@ -6,8 +6,17 @@ const web3 = getWeb3();
 
 function createAccount() {
     console.log("createAccount");
-    web3.eth.personal.newAccount('qwerfdsa')
+    let cmdPassword = getPassword();
+    let password = cmdPassword ? cmdPassword : 'qwerfdsaesdtgrhf';
+    web3.eth.personal.newAccount(password)
         .then(console.log);
+}
+
+function getPassword() {
+    if (process.argv.length > 4) {
+        return process.argv[4];
+    }
+    return undefined;
 }
 
 createAccount();
