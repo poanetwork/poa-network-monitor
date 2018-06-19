b17eb8714793745c411e8cea9babf5ec15400dbe
Natalia Gulko <natalia.gulko@yahoo.com>
2018-06-19 12:21:46 -0700

    Fixed core runs fail without keystore file

--- common/config.js
+++ common/config.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
 const toml = require('toml');
 const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
-let keyStore = fs.readFileSync(config["keyStorePath_" + getNetworkName()]);
+let keyStore;
+try {
+    keyStore = fs.readFileSync(config["keyStorePath_" + getNetworkName()]);
+} catch (error) {
+    console.log("error in reading keyStore file: " + error);
+}
+
 
 function getNetworkName() {
     let network = "";
     if (process.argv.length > 2) {
         switch (process.argv[2]) {
             case 'sokol':
-                network =  'sokol';
+                network = 'sokol';
                 break;
             case 'core':
-                network =  'core';
+                network = 'core';
                 break;
             default:
-                network =  config.network;
+                network = config.network;
         }
     }
     else {
