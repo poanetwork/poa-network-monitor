cd87dccbf12cb69df4d552a77e0ab548e0044e67
Natalia Gulko <natalia.gulko@yahoo.com>
2018-07-02 01:49:13 -0700

    Removed url from the monitor scripts

--- scripts/monitor-core.sh
+++ scripts/monitor-core.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $POA_MONITOR_PATH
-node $POA_MONITOR_PATH/test-result-monitor.js core http://localhost:8541 1800 >> $POA_MONITOR_PATH/logs/monitor-core-log 2>&1;
+node $POA_MONITOR_PATH/test-result-monitor.js core 1800 >> $POA_MONITOR_PATH/logs/monitor-core-log 2>&1;
 
--- scripts/monitor-sokol.sh
+++ scripts/monitor-sokol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $POA_MONITOR_PATH
-node  $POA_MONITOR_PATH/test-result-monitor.js sokol http://localhost:8540 1800 >> $POA_MONITOR_PATH/logs/monitor-sokol-log 2>&1;
+node  $POA_MONITOR_PATH/test-result-monitor.js sokol 1800 >> $POA_MONITOR_PATH/logs/monitor-sokol-log 2>&1;
 
